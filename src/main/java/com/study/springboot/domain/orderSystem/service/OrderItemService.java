66b23add6592dda047bdee55fa93ccc6939321de
yubin-im <ybyb130@gmail.com>
2024-04-29 10:05:01 +0900

    feat: 메뉴 개수 추가 기능 구현 중

--- src/main/java/com/study/springboot/domain/orderSystem/service/OrderItemService.java
+++ src/main/java/com/study/springboot/domain/orderSystem/service/OrderItemService.java
@@ -1,12 +1,35 @@
 package com.study.springboot.domain.orderSystem.service;
 
 
+import com.study.springboot.domain.orderSystem.OrderItem;
+import com.study.springboot.domain.orderSystem.OrderList;
 import com.study.springboot.domain.orderSystem.repository.OrderItemRepository;
+import com.study.springboot.domain.product.Product;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class OrderItemService {
     private final OrderItemRepository orderItemRepository;
+
+    // 상품 개수 1개 추가
+    @Transactional
+    public Integer addAmount(OrderList orderList, Product product) {
+        OrderItem orderItem = orderItemRepository.findOrderItemByOrderListAndProduct(orderList, product);
+
+        Integer orderAmount = orderItem.getOrderAmount() + 1;
+        return orderAmount;
+    }
+
+    // 상품 개수 1개 삭제
+    @Transactional
+    public Integer removeAmount(OrderList orderList, Product product) {
+        OrderItem orderItem = orderItemRepository.findOrderItemByOrderListAndProduct(orderList, product);
+
+        Integer orderAmount = orderItem.getOrderAmount() - 1;
+        return orderAmount;
+    }
+
 }
