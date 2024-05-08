21ecff89b8d6a9d54c41a82c96b7775e4a2c8d0d
yubin-im <ybyb130@gmail.com>
2024-05-08 11:34:46 +0900

    pull from dev

---
+++ src/main/generated/com/study/springboot/domain/product/QProduct.java
@@ -0,0 +1,49 @@
+package com.study.springboot.domain.product;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QProduct is a Querydsl query type for Product
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QProduct extends EntityPathBase<Product> {
+
+    private static final long serialVersionUID = 337827747L;
+
+    public static final QProduct product = new QProduct("product");
+
+    public final EnumPath<com.study.springboot.enumeration.ProductCategory> category = createEnum("category", com.study.springboot.enumeration.ProductCategory.class);
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath productCode = createString("productCode");
+
+    public final BooleanPath productDelYn = createBoolean("productDelYn");
+
+    public final StringPath productImgUrl = createString("productImgUrl");
+
+    public final StringPath productName = createString("productName");
+
+    public final NumberPath<Integer> productPrice = createNumber("productPrice", Integer.class);
+
+    public QProduct(String variable) {
+        super(Product.class, forVariable(variable));
+    }
+
+    public QProduct(Path<? extends Product> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QProduct(PathMetadata metadata) {
+        super(Product.class, metadata);
+    }
+
+}
+
---
+++ src/main/generated/com/study/springboot/domain/user/QUser.java
@@ -0,0 +1,54 @@
+package com.study.springboot.domain.user;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QUser is a Querydsl query type for User
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QUser extends EntityPathBase<User> {
+
+    private static final long serialVersionUID = 591274817L;
+
+    public static final QUser user = new QUser("user");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final ListPath<com.study.springboot.domain.orderSystem.OrderList, com.study.springboot.domain.orderSystem.QOrderList> orderList = this.<com.study.springboot.domain.orderSystem.OrderList, com.study.springboot.domain.orderSystem.QOrderList>createList("orderList", com.study.springboot.domain.orderSystem.OrderList.class, com.study.springboot.domain.orderSystem.QOrderList.class, PathInits.DIRECT2);
+
+    public final BooleanPath userDelYn = createBoolean("userDelYn");
+
+    public final StringPath userId = createString("userId");
+
+    public final DatePath<java.time.LocalDate> userJoinDate = createDate("userJoinDate", java.time.LocalDate.class);
+
+    public final StringPath userName = createString("userName");
+
+    public final NumberPath<Integer> userPoint = createNumber("userPoint", Integer.class);
+
+    public final StringPath userPw = createString("userPw");
+
+    public final EnumPath<com.study.springboot.enumeration.UserRole> userRole = createEnum("userRole", com.study.springboot.enumeration.UserRole.class);
+
+    public QUser(String variable) {
+        super(User.class, forVariable(variable));
+    }
+
+    public QUser(Path<? extends User> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QUser(PathMetadata metadata) {
+        super(User.class, metadata);
+    }
+
+}
+
